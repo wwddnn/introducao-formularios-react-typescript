1957482aa91c2530b63d4f8d0f237f19ef432f15
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-07-13 22:59:42 -0300

    Introducao a formularios React + Typescript

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+type FormData = {
+  firstName: string,
+  lastName: string
+}
+
+function App() {
+
+  const [fullName, setFullName] = useState<string>();
+
+  const [formData, setFormData] = useState<FormData>({ /* é de boa prática, colocar os campos pra iniciarem vazios*/
+    firstName: '',
+    lastName: ''
+  });
+
+  function handleInputChange(event : any) {
+    const value = event.target.value; /* valor que o usuario digitou no campo, se digitou joao é esse valor que sera capturado*/
+    const name = event.target.name; /* nome do campo do input que foi alterado*/
+    console.log("campo alterado", name);
+    console.log("campo alterado", value);
+    setFormData({...formData, [name]: value}); /* aproveita os dados que estao no formulario*//* aproveita os dados que estao no formulario*/
+  }
+
+  function handleFormSubmit(event: any) {
+    event.preventDefault();
+    setFullName(formData.firstName + " " + formData.lastName);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}> /* onSubmit intercepta o evento submit do botao*/
+        <div>
+          <input 
+            name="firstName"
+            value={formData.firstName}
+            type="text" 
+            placeholder="Digite seu nome"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <input 
+            name="lastName"
+            value={formData.lastName}
+            type="text" 
+            placeholder="Digite o sobrenome"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <button type="submit">Mostrar nome completo</button>
+        </div>
+      </form>
+      <h2>{fullName}</h2>
+    </>
+  );
+}
+
+export default App
